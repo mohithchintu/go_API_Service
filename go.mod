da02b77b3f795cec32206d4340d436d269a91c07
mohithchintu <konireddynagamohithreddy@gmail.com>
2024-08-01 13:10:28 +0530

    Second Commit

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module go_api_service
 
 go 1.22.5
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello")
+	godotenv.Load()
+
+	Port := os.Getenv("PORT")
+
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + Port,
+	}
+	log.Printf("Server running on port %v", Port)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
